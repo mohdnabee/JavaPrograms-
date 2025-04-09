bccd311e78a7a786e7f3976f7bbec0f41bf92870
Mohdnabee <mohdnabeel989@gamil.com>
2025-04-09 13:17:05 +0530

    3 day tra

--- operators.java
+++ operators.java
@@ -1,8 +1,42 @@
-public class modulas {
+import java.util.*;
+
+public class operators {
     public static void main(String[] args) {
 
-        int a = 23 ; 
-        int b = 15; 
-        System.out.println("remander =" + a%b);
+        Scanner in = new Scanner(System.in); 
+        System.out.println("Enter two numbers: ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        System.out.println("Enter the opertaion +, -, *, /,% ");
+        char operator =  in.next().charAt(0);
+   switch (operator) {
+       case '+':
+       System.out.println("Addition : "+(a+b) );
+       break;
+       case '-':
+       System.out.println("Subtraction : " + (a-b));
+           break;
+           case '*':
+      System.out.println("Multiplication : " +(a*b));
+               break;
+               case '/' :
+               if(b == 0) {
+                System.out.println("Invalid Division");
+            } else {
+                System.out.println("divison : "+(a/b));
+            }
+            break;
+               case '%':if(b == 0) {
+                System.out.println("Invalid Division");
+            } else {
+                System.out.println("Modulus : "+ (a%b));
+            }    
+               break;
+       default:
+         System.out.println("inavild operator");
+   }
+        
     }
+
 }
--- questionDay2.java
+++ questionDay2.java
@@ -7,7 +7,7 @@ public class questionDay2 {
         int b = 5 ; 
 
         Scanner sc = new Scanner(System.in); 
-        System.out.println("Enter the opertaion +, -, *, / ");
+        System.out.println("Enter the opertaion +, -, *, /,% ");
         char operator =  sc.next().charAt(0);
 
         if (operator == '+'){
@@ -18,7 +18,10 @@ public class questionDay2 {
             System.out.println("Multiplication : " +(a*b));
         }else if (operator == '/'){
             System.out.println("divison : "+(a/b));
-        }else {
+        }else if(operator == '%'){
+            System.out.println("Modulus : "+ (a%b));
+        }
+        else {
             System.out.println("invaild operator" );
         }
     }

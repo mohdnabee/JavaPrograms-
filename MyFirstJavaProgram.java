a5f851388c7960af9c2cffd5852103daa0a6ad92
Mohdnabee <mohdnabeel989@gamil.com>
2025-05-06 23:46:49 +0530

    check

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,15 @@
+public class MyFirstJavaProgram {
+
+    /* This is my first java program.
+     * This will print 'Hello World' as the output
+     */
+ 
+    public static void main(String []args) {
+
+      //  System.out.println("Hello World"); // prints Hello World
+       for (int i=1;i<=10;i++){
+         System.out.println("Hello World");
+i++;
+       }
+    }
+ }
---
+++ mortal.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class mortal {
+
+    public static void main(String[] args) {
+        
+        System.out.println("WELLCOME TO MORTAL  KOMBAT");
+
+        Scanner mortal = new Scanner(System.in);
+
+
+
+    }
+}
